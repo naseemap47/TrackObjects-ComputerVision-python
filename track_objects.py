3b9395941d692e75b42f774ac35fe119af8288e8
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-06 11:19:25 +0530

    Calculated FPS and put on display

--- track_objects.py
+++ track_objects.py
@@ -1,11 +1,22 @@
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
-
+p_time = 0
 while True:
     success, img = cap.read()
 
+    # FPS
+    c_time = time.time()
+    fps = 1 / (c_time - p_time)
+    p_time = c_time
+    cv2.putText(
+        img, f'FPS: {int(fps)}', (10, 70),
+        cv2.FONT_HERSHEY_PLAIN, 2,
+        (255, 0, 255), 2
+    )
     cv2.imshow('Webcam', img)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
