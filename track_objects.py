6d4232a035482617601a34dbd5132aa9d2775e06
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-06 11:51:33 +0530

    draw bound box in Selection window, press enter and it will keep on tracking, after selection, destroy selection window, put text status on window

--- track_objects.py
+++ track_objects.py
@@ -6,12 +6,38 @@ cap = cv2.VideoCapture(0)
 # Tracker
 tracker = cv2.legacy.TrackerMOSSE_create()
 success, img = cap.read()
-bound_box = cv2.selectROI('webcam', img, False)
+bound_box = cv2.selectROI('Selection', img, False)
 tracker.init(img, bound_box)
+cv2.destroyWindow('Selection')
+
+
+def drawBox(image, box):
+    x, y, w, h = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+    cv2.rectangle(
+        image, (x, y), (x+w, y+h),
+        (0, 255, 0), 2
+    )
+    cv2.putText(
+        img, "Tracking", (10, 90),
+        cv2.FONT_HERSHEY_PLAIN, 2,
+        (0, 255, 0), 2
+    )
+
 
 p_time = 0
 while True:
     success, img = cap.read()
+    success, bound_box = tracker.update(img)
+    # print(bound_box)
+
+    if success:
+        drawBox(img, bound_box)
+    else:
+        cv2.putText(
+            img, "Lost", (10, 90),
+            cv2.FONT_HERSHEY_PLAIN, 2,
+            (0, 0, 255), 2
+        )
 
     # FPS
     c_time = time.time()
